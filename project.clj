045ff28d24f219f3c33887fcc701bee5b504d15c
Henry Garner <henry@henrygarner.com>
2012-08-08 19:37:59 +0100

    Creating a test neural network

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.encog/encog-core "3.1.0"]
+                 [clojure-encog "0.4.0-SNAPSHOT"]])
--- src/entendre/core.clj
+++ src/entendre/core.clj
@@ -1,6 +1,17 @@
-(ns entendre.core)
+(ns entendre.core
+  (:use [clojure-encog.nnets]
+        [clojure-encog.training]
+        [clojure-encog.normalization])
+  (:import [org.encog.ml.train.strategy RequiredImprovementStrategy]))
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+(defn -main []
+  (let [network (make-network {:input   2
+                               :output  1
+                               :hidden [50 10]}
+                              (make-activationF :sigmoid)
+                              (make-pattern     :feed-forward))
+        xor-input [[0.0 0.0] [1.0 0.0] [0.0 0.1] [1.0 1.0]]
+        xor-ideal [[0.0] [1.0] [1.0] [0.0]]
+        dataset   (make-data :basic-dataset xor-input xor-ideal)
+        trainer   ((make-trainer :back-prop) network dataset)]
+    (train trainer 0.01 500 [(RequiredImprovementStrategy. 5)])))
